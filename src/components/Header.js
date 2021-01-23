5d24cd92678b1c7a3506054084227d1cd93cb9d8
Oleg Saveniuk <oleg.saveniuk@gmail.com>
2021-01-23 22:37:30 +0200

    Add css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
-import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
-  return <div></div>;
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <button>Add</button>
+    </header>
+  );
 };
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 
 export default Header;
